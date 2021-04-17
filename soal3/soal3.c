385adfcbea96888d716e4e7c059b9995f2cbb7f7
fitrianazt <73879144+fitrianazt@users.noreply.github.com>
2021-04-17 21:25:46 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -9,15 +9,14 @@ int main() {
 	pid_t pid;
 	char curtime[100];
 	time_t t1 = time(NULL);
-	struct tm*p1 = localtime(&t1);
-	strftime (curtime, 100, "%Y-%m-%d_%H:%M:%S", p1);
+	struct tm*pid1 = localtime(&t1);
+	
+	strftime (curtime, 100, "%Y-%m-%d_%H:%M:%S", pid1);
 
 	pid = fork ();
-	if (pid < 0) exit(0);
-	if (pid > 0) exit (0);
-		if (cid == 0){
-			char *agv[] = {"mkdir", curtime, NULL};
-			execv ("/bin/mkdir", agv);
-		}
+	if (pid == 0){
+		char *agv[] = {"mkdir", curtime, NULL};
+		execv ("/bin/mkdir", agv);
+	}
 	sleep(40);
 }
